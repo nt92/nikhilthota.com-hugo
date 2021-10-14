45fc9130913cdafd603041fcc6cef61f67938fec
nt92 <nikhilthota@fb.com>
2021-10-14 07:37:48 -0700

    added post link copy (experimental)

--- themes/custom/static/js/custom.js
+++ themes/custom/static/js/custom.js
@@ -1,8 +1,1 @@
-// Prevent copying images
-// document.addEventListener('contextmenu', event => {
-//     if (event.target.tagName === 'IMG') {
-//         event.preventDefault()
-//     }
-// });
-
 console.log('Hello, Console Wizard :)')
